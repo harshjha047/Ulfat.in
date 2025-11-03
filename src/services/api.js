77e9ba48c638cebcc603906b42b888b74fd3d235
harshjha047 <spyderhj11@gmail.com>
2025-11-03 21:58:39 +0530

    0.0.2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: `https://ulfat.onrender.com/api`,
   withCredentials: true,
 });
 console.log(import.meta.env.VITE_API_BASE_URL);
