955022cb4b67a8bcdf17ea8daf81c2290cfc0ce6
harshjha047 <spyderhj11@gmail.com>
2025-11-24 23:04:01 +0530

    0.1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import MainNavigationBar from './Components/Nav/MainNavigationBar'
-import MainFooter from './Components/Footer/MainFooter'
-import { Toaster } from 'react-hot-toast'
-import Menu from './Components/Menu/Menu'
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import MainNavigationBar from "./Components/Nav/MainNavigationBar";
+import MainFooter from "./Components/Footer/MainFooter";
+import { Toaster } from "react-hot-toast";
+import Menu from "./Components/Menu/Menu";
+import ReactGA from "react-ga4";
 
 function App() {
-  return (<>
-  <div className="bg-[#fffdfa]">
-    <MainNavigationBar/>
-      <Toaster />
-      <Menu/>
-    <Outlet/>
-    <MainFooter/>
-    </div>
-
+  useEffect(() => {
+    ReactGA.initialize("G-E2KPCSKD5V");
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.path,
+      title: "App.jsx",
+    });
+  }, []);
+  return (
+    <>
+      <div className="bg-[#fffdfa]">
+        <MainNavigationBar />
+        <Toaster />
+        <Menu />
+        <Outlet />
+        <MainFooter />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
